1b42da064c25fa2742644dafe49e0e3b01f0c86b
riteshkoranga <riteshkoranga5@gmail.com>
2024-01-29 12:53:11 +0530

    add navbar

---
+++ app/navbar.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <nav>
+        <Link href="/">Logo</Link>
+        <ul>
+            <li><Link href="/">Dashboard</Link></li>
+            <li><Link href="/issues">Issues</Link></li>
+        </ul>
+    </nav>
+    
+  )
+}
+
+export default navbar
